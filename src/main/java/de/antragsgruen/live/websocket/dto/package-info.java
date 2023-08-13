168d9ac1313cc8fe8a23e1c3bb7e495112d38286
Tobias Hößl <tobias@hoessl.eu>
2023-08-13 09:23:48 +0200

    Switch packages to NonNull

---
+++ src/main/java/de/antragsgruen/live/websocket/dto/package-info.java
@@ -0,0 +1,6 @@
+@NonNullApi
+@NonNullFields
+package de.antragsgruen.live.websocket.dto;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
---
+++ src/test/java/de/antragsgruen/live/utils/package-info.java
@@ -0,0 +1,6 @@
+@NonNullApi
+@NonNullFields
+package de.antragsgruen.live.utils;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
