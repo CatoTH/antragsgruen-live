ba38affd930afe62356d31860c971a64268566c5
Tobias Hößl <tobias@hoessl.eu>
2023-08-15 19:14:30 +0200

    Send SpeechEvents only to relevant users

--- src/main/java/de/antragsgruen/live/SpeechUserHandler.java
+++ src/main/java/de/antragsgruen/live/SpeechUserHandler.java
@@ -7,8 +7,7 @@ import de.antragsgruen.live.websocket.dto.WSSpeechQueue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.simp.user.SimpUser;
-import org.springframework.messaging.simp.user.SimpUserRegistry;
+import org.springframework.messaging.simp.user.*;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,13 +20,30 @@ public class SpeechUserHandler {
 
     private final Logger logger = LoggerFactory.getLogger(SpeechUserHandler.class);
 
+    private String[] findRelevantUserIds(String subdomain, String consultation)
+    {
+        // 1) First find all subscriptions with a destination matching /user/[site]/[consultation]/[userid]/speech
+        // 2) Extract the User ID from the destinations
+        // 3) Return unique User IDs (we only need to send messages to each user once)
+        return userRegistry.findSubscriptions(subscription -> {
+            String[] parts = subscription.getDestination().split("/");
+            return parts.length == 6 && "user".equals(parts[1]) && "speech".equals(parts[5])
+                    && subdomain.equals(parts[2]) && consultation.equals(parts[3]);
+        }).stream().map(subscription -> {
+            String[] parts = subscription.getDestination().split("/");
+            return parts[4];
+        }).distinct().toArray(String[]::new);
+    }
+
     public void onSpeechEvent(String subdomain, String consultation, MQSpeechQueue mqQueue) {
-        logger.info("Sending speech event to " + userRegistry.getUserCount() + " user(s)");
+        String[] users = findRelevantUserIds(subdomain, consultation);
 
-        for (SimpUser user : userRegistry.getUsers()) {
-            WSSpeechQueue wsQueue = SpeechUserMapper.convertQueue(mqQueue, user.getName());
+        logger.info("Sending speech event to " + users.length + " (out of " + userRegistry.getUserCount() + ") user(s)");
 
-            sender.sendToUser(subdomain, consultation, user.getName(), Sender.USER_CHANNEL_SPEECH, wsQueue);
+        for (String userId : users) {
+            WSSpeechQueue wsQueue = SpeechUserMapper.convertQueue(mqQueue, userId);
+
+            sender.sendToUser(subdomain, consultation, userId, Sender.USER_CHANNEL_SPEECH, wsQueue);
         }
     }
 }
