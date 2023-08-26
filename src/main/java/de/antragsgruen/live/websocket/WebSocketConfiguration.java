5e0282e166b3bc39ae421398071432122c4d9e8b
Tobias Hößl <tobias@hoessl.eu>
2023-08-26 18:04:01 +0200

    Set up Websocket heartbeats, to prevent timeouts with proxies

--- src/main/java/de/antragsgruen/live/websocket/WebSocketConfiguration.java
+++ src/main/java/de/antragsgruen/live/websocket/WebSocketConfiguration.java
@@ -5,8 +5,10 @@ import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -22,15 +24,28 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
         WSSpeechSubqueueUserItem.class,
 })
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Value("${antragsgruen.ws.heartbeat}")
+    private long wsHeartbeatInterval;
+
     @Value("${antragsgruen.ws.origins}")
     private String wsOriginPatterns;
 
     @Autowired
     private WebsocketChannelInterceptor interceptor;
 
+    private TaskScheduler messageBrokerTaskScheduler;
+
+    @Autowired
+    public void setMessageBrokerTaskScheduler(@Lazy TaskScheduler taskScheduler) {
+        this.messageBrokerTaskScheduler = taskScheduler;
+    }
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/user", "/admin", "/topic", "/queue");
+        config
+                .enableSimpleBroker("/user", "/admin", "/topic", "/queue")
+                .setHeartbeatValue(new long[]{wsHeartbeatInterval, wsHeartbeatInterval})
+                .setTaskScheduler(messageBrokerTaskScheduler);
         config.setApplicationDestinationPrefixes("/app");
     }
 
