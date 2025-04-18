b8ab795e362f63d5bce9ac5043e46ccdc63458f8
Tobias Hößl <tobias@hoessl.eu>
2025-04-18 16:50:37 -0400

    More work on agenda integration

--- src/main/java/de/antragsgruen/live/mapper/AgendaItemMapper.java
+++ src/main/java/de/antragsgruen/live/mapper/AgendaItemMapper.java
@@ -3,12 +3,21 @@ package de.antragsgruen.live.mapper;
 import de.antragsgruen.live.rabbitmq.dto.MQAgendaItem;
 import de.antragsgruen.live.websocket.dto.WSAgendaItem;
 
+import java.util.stream.Stream;
+
 public final class AgendaItemMapper {
     private AgendaItemMapper() {
         throw new UnsupportedOperationException();
     }
 
-    public static WSSAgendaItem[] convertAgendaItems(MQAgendaItem[] items) {
-        // @TOOD
+    public static WSAgendaItem[] convertAgendaItems(MQAgendaItem[] items) {
+        return Stream
+                .of(items)
+                .map(AgendaItemMapper::convertAgendaItem)
+                .toArray(WSAgendaItem[]::new);
+    }
+
+    private static WSAgendaItem convertAgendaItem(MQAgendaItem item) {
+
     }
 }
