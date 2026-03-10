1bb29e59d38c6b888cf8731e21e609dde7393f07
Tobias Hößl <tobias@hoessl.eu>
2026-03-10 18:50:45 -0400

    Fix JacksonJsonMessageConverter

--- src/main/java/de/antragsgruen/live/rabbitmq/configuration/MessagesConfiguration.java
+++ src/main/java/de/antragsgruen/live/rabbitmq/configuration/MessagesConfiguration.java
@@ -1,6 +1,6 @@
 package de.antragsgruen.live.rabbitmq.configuration;
 
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +9,6 @@ import org.springframework.context.annotation.Configuration;
 public class MessagesConfiguration {
     @Bean
     public MessageConverter converter() {
-        return new Jackson2JsonMessageConverter();
+        return new JacksonJsonMessageConverter();
     }
 }
